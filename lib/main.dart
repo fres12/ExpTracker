ab46f0c9190a1e19f93ee81d08275c1df4c832c4
I Gusti Ngurah Adhya Pradipta <88218769+ayhda@users.noreply.github.com>
2024-11-20 14:44:24 +0700

    beranda
    beranda

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
-import 'package:flutter/material.dart';
-import '../widget/navigation_menu.dart';
+import 'package:flutter/material.dart'; // Pastikan ini diimpor
+import 'beranda.dart'; // Impor file beranda.dart Anda
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp()); // Fungsi runApp sudah didefinisikan di sini
 }
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ExpTracker',
-      home: const NavigationMenu(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'Inter',
+        textTheme: TextTheme(
+          displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
+          displayMedium: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+          bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
+          bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
+        ),
+      ),
+      home: const Beranda(), // Menggunakan class Beranda dari file beranda.dart
     );
   }
 }

63168adbcfca7bf1eba33bf99a933527af54ff38
I Gusti Ngurah Adhya Pradipta <88218769+ayhda@users.noreply.github.com>
2024-11-21 08:03:12 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,16 @@
-import 'package:flutter/material.dart'; // Pastikan ini diimpor
-import 'beranda.dart'; // Impor file beranda.dart Anda
+import 'package:flutter/material.dart';
+import '../widget/navigation_menu.dart';
 
 void main() {
-  runApp(const MyApp()); // Fungsi runApp sudah didefinisikan di sini
+  runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        fontFamily: 'Inter',
-        textTheme: TextTheme(
-          displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.w700),
-          displayMedium: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-          bodyLarge: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-          bodyMedium: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
-        ),
-      ),
-      home: const Beranda(), // Menggunakan class Beranda dari file beranda.dart
+      title: 'ExpTracker',
+      home: const NavigationMenu(),
+      debugShowCheckedModeBanner: false, // Menyembunyikan label "Debug"
     );
   }
 }

ce58e4f87f27854552e3484615557269333ab29c
fres12 <fresnelsiregar11@gmail.com>
2024-11-21 20:17:56 +0700

    Masukin detail

--- lib/screen/list.dart
+++ lib/screen/list.dart
@@ -1,3 +1,5 @@
+import 'package:cobacoba/screen/detail.dart';
+
 import '../style/hover_tile.dart';
 import '../style/settings.dart';
 import '../widget/searchbar.dart';
@@ -6,52 +8,53 @@ import '../style/app_style.dart';
 import '../data/data_item.dart';
 import 'package:iconsax_flutter/iconsax_flutter.dart';
 import '../style/functionstyle.dart';
+import '../screen/tambah.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: TColors.background,
-    appBar: AppBar(
-      automaticallyImplyLeading: false, // Hilangkan back button default
-      toolbarHeight: 71, 
-      backgroundColor: Colors.white,
-      elevation: 0, 
-      flexibleSpace: Padding(
-        padding: TPosition.topnavbar,
-        child: Stack(
-          children: [
-            // Teks di tengah
-            Align(
-              alignment: Alignment.center,
-              child: Text(
-                'List Item',
-                style: AppStyle.header,
+    return Scaffold(
+      backgroundColor: TColors.background,
+      appBar: AppBar(
+        automaticallyImplyLeading: false, // Hilangkan back button default
+        toolbarHeight: 71,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        flexibleSpace: Padding(
+          padding: TPosition.topnavbar,
+          child: Stack(
+            children: [
+              // Teks di tengah
+              Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'List Item',
+                  style: AppStyle.header,
+                ),
               ),
-            ),
-            // Ikon di kanan atas
-            Align(
-              alignment: Alignment.centerRight,
-              child: IconButton(
-                icon: const Icon(Icons.add),
-                onPressed: () {
-                  // Aksi untuk tombol tambah
-                  
-                  // Tambahkan aksi untuk ikon
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TambahScreen()),
-                  );
-                },
+              // Ikon di kanan atas
+              Align(
+                alignment: Alignment.centerRight,
+                child: IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    // Aksi untuk tombol tambah
+
+                    // Tambahkan aksi untuk ikon
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => TambahScreen()),
+                    );
+                  },
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
-    ),
-    body: SingleChildScrollView(
+      body: SingleChildScrollView(
         // Membuat body dapat discroll
         child: Container(
           margin: TPosition.marginPageDefault,
@@ -85,7 +88,12 @@ class ListScreen extends StatelessWidget {
                   itemBuilder: (context, index) {
                     return HoverableTile(
                       padding: TPosition.listItem, // Jarak antar item
-                      onTap: () {}, //tampilkan detail
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => DetailItemPage()),
+                    );
+                  }, //tampilkan detail
                       title: Text(
                         item[index].nama_item, // Judul
                         overflow: TextOverflow.ellipsis, //Menambah elipsis\
@@ -102,7 +110,8 @@ class ListScreen extends StatelessWidget {
                         mainAxisSize:
                             MainAxisSize.min, // Mengurangi ruang di kanan
                         children: [
-                          Text(item[index].sisa, // Sisa hari
+                          Text(item[index].sisa, 
+                          overflow: TextOverflow.ellipsis,// Sisa hari
                               style: getStyleForSisa(item[index].sisanya)),
                           const SizedBox(
                               width: 10), // Jarak antara sisa hari dan ikon
